4f8e8d0068799b027d4b8166db1b628fd7b22365
caranicas <kendrick.caranicas@gmail.com>
2022-09-21 11:38:29 -0400

    added routing as an option

---
+++ src/pages/settingsPage/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "@tanstack/react-location";
+export default function SettingsPage() {
+  return (
+    <div>
+      <h1>Settings Page</h1>
+      <Link to="/">I will navigate to HOME</Link>
+    </div>
+  );
+}
