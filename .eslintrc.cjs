b7080019c81721c5107b9a85a7ff29a129591301
caranicas <kendrick.caranicas@gmail.com>
2022-09-21 12:31:42 -0400

    add lint

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    // tsconfigRootDir: __dirname,
+  },
+  plugins: ['react', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    // "quotes": ["error", "single"],
+    semi: ['error', 'always'],
+  },
+  overrides: [],
+};
